8e1d987b64f34b0253a092a26c61a40e739d7459
hmdsg <hmdsg@gmail.com>
2020-09-28 12:12:44 +0000

    wip

--- get_token.py
+++ get_token.py
@@ -17,7 +17,7 @@ def get_token():
     payload = {"APIPassword": API_PASSWORD}
 
     try:
-        response = requests.post(API_URL, data=json.dumps(payload).encode("utf8"), headers=headers)  
+        response = requests.post(URL, data=json.dumps(payload).encode("utf8"), headers=headers)  
         token = json.loads(response.text).get("Token")
 
     except Exception as e:
@@ -27,15 +27,25 @@ def get_token():
 
 def get_board(token):
 
-    URL = API_URL + "/board/5401@1"
+    URL = API_URL + "/board/9433@1"
     headers = {
         "content-type": "application/json",
         "X-API-KEY": token
     }
 
     try:
-        response = requests.post(API_URL, data=json.dumps(payload).encode("utf8"), headers=headers)  
+        response = requests.get(URL, headers=headers)  
     except Exception as e:
         print (e)
     
     return json.loads(response.text)
+
+print ("始めます")
+
+token = get_token()
+
+print (token)
+
+res = get_board(token)
+
+print (res)
